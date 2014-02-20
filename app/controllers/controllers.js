0f8c460b4ed7841343fff6ca424a28c5abaeffaa
romain <romain@romain.local>
2014-02-20 12:32:28 +0100

    supp noticesFactory + modifs noticesCtrl

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -11,16 +11,41 @@ mnlt.config(function($httpProvider) {
 // noticesFactory
 mnlt.factory("noticesFactory", function($http, $q) {
 	return {
+		// Get all notices
 		getNotices: function() {
 			return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&count=-1&callback=JSON_CALLBACK");
+		},
+		// Get all "Grandes Dates" notices
+		getGrandesDates: function() {
+			return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&rubriques=grandes-dates&count=-1&callback=JSON_CALLBACK");
+		},
+		// Get all "Histoire" notices
+		getHistoire: function() {
+			return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&rubriques=histoire&count=-1&callback=JSON_CALLBACK");
+		},
+		// Get all "Personnalités" notices
+		getPersonnalites: function() {
+			return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&rubriques=personnalites&count=-1&callback=JSON_CALLBACK");
+		},
+		// Get all "Thèmes" notices
+		getThemes: function() {
+			return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&rubriques=themes&count=-1&callback=JSON_CALLBACK");
+		},
+		// Get all "Art - Patrimoine" notices
+		getArtPatrimoine: function() {
+			return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&rubriques=art-patrimoine&count=-1&callback=JSON_CALLBACK");
 		}
 	};
 });
 
 
 // noticesCtrl
-mnlt.controller("noticesCtrl", noticesCtrl = function($scope, noticesFactory) {
-	return noticesFactory.getNotices().success(function(data) {
-		return $scope.notices = data.posts;
-	});
+mnlt.controller("noticesCtrl", noticesCtrl = function($scope, $http) {
+
+	$scope.catName = function(name) {
+        return $http.jsonp("http://mvpf.phpnet.org/museevirtuelprotestant/api/get_posts/?post_type=notice&rubriques=" + name + "&count=-1&callback=JSON_CALLBACK").success(function(data) {
+			return $scope.notices = data.posts;
+		});
+    };
+
 });
